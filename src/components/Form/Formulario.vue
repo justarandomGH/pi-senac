d05a33867bb1c878ded94e11780679e16849762b
justarandomGH <105462149+justarandomGH@users.noreply.github.com>
2023-02-16 17:11:00 -0300

    Update Formulario.vue

--- src/components/Form/Formulario.vue
+++ src/components/Form/Formulario.vue
@@ -14,4 +14,33 @@ export default {
 </script>
 
 <style scoped>
+</style><template>
+  <div>
+    <HeaderForm />
+    <div class="form-box">
+      <Background />
+      <ContactForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderForm from "./HeaderForm.vue";
+import ContactForm from "./ContactForm.vue";
+import Background from "./Background.vue";
+export default {
+  name: "Formulario",
+
+  components: {
+    HeaderForm,
+    Background,
+    ContactForm,
+  },
+};
+</script>
+
+<style scoped>
+.form-box {
+  display: flex;
+}
 </style>
