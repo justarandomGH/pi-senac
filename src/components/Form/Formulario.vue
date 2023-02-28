1373a32b44a48068e4506996e5eeeafcfff164d6
justarandomGH <105462149+justarandomGH@users.noreply.github.com>
2023-02-28 15:11:38 -0300

    Update Formulario.vue

--- src/components/Form/Formulario.vue
+++ src/components/Form/Formulario.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <header>
-      <img src="https://cdn-icons-png.flaticon.com/512/507/507257.png" alt="" />
-      <img src="https://cdn-icons-png.flaticon.com/512/860/860790.png" alt="" />
-    </header>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "Formulario",
-};
-</script>
-
-<style scoped>
-</style><template>
-  <div>
     <HeaderForm />
     <div class="form-box">
       <Background />
